4c2fe993d9e3e59b3bcf45cb694c6af865095c6f
Unknown <danteklijn01@gmail.com>
2019-04-02 11:02:37 +0200

    Les system toe gevoegt
    alleen nog het huiswerk op geef systeem en dan is hij klaar

---
+++ Your_a_wizzard_harry/App_Code/SQLFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Summary description for SQLFunctions
+/// </summary>
+public class SQLFunctions
+{
+    //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Harry_Potter.mdf;Integrated Security=True";
+
+    //string provider = "System.Data.SqlClient";
+
+    //Database db = Database.OpenConnectionString(connectionString, provider);
+
+    //public SQLFunctions()
+    //{
+      
+    //}
+    //public string GetLessonStuff()
+    //{
+    //    var GetTeacher = "SELECT Teacher FROM Lesson WHERE Selected_Lesson = @0";
+    //    string Teacher = db.QueryValue(GetTeacher, 1);
+    //    return 
+    //}
+}
